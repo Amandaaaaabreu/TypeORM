231e0d26c6e8e839019643b9e26e8ee7fc68b1e8
Amandaaaaabreu <amandaabreu045@gmail.com>
2025-06-27 20:14:16 -0300

    adicionando primeira rota

--- index.ts
+++ index.ts
@@ -1,6 +1,50 @@
+// import express, { Request, Response, Router } from 'express';
 
-function soma(numero: number){
-    return numero +1
-}
+// const server = express();
+// const routes = Router();
 
-console.log(soma(5));
+// routes.get('/', (req: Request, res: Response) => {
+//   res.json({
+//     message: 'Hello World'
+//   });
+// });
+
+// server.use(routes);
+
+// server.listen(5000, () => {
+//   console.log('Server is running on port 5000');
+// });
+
+// ou
+
+import express, { Request, Response } from 'express';
+
+const server = express();
+
+server.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Bem vindo a API' });
+});
+
+server.listen(5000, () => {
+  console.log('Server rodando na porta 5000 http://localhost:5000/');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//GET - Ler 
+//POST - Criar
+//PUT - Atualizar
+//PATCH - Atualizar uma informação específica
+//DELETE - Remover
+//UPDATE - Atualizar
