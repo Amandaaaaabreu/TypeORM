a030f0b5ea291787dbf492e3d7073a048b021f01
Amandaaaaabreu <amandaabreu045@gmail.com>
2025-07-19 00:21:46 -0300

    criando testes na camada de service

--- src/services/GetAllUserService.test.ts
+++ src/services/GetAllUserService.test.ts
@@ -1,5 +1,6 @@
 import createConnection from '../database';
 import { getConnection } from 'typeorm';
+import {v4 as uuid} from 'uuid';
 import { GetAllUserService } from './GetAllUserService';
 import { CreateUserService } from './CreateUserService';
 
@@ -15,13 +16,41 @@ describe('GetAllUserService', () => {
 
     afterAll(async () => {
         const connection = getConnection();
-        await connection.query('DELETE FROM users');
+        await connection.query('DELETE FROM usuarios');
         await connection.close();
     
     })
-
     
     it('Deve retornar todos os usuários cadastrados', async () => {
+        
+        const createUserService = new CreateUserService();
 
+        await createUserService.execute({
+            id: uuid(),
+            nome: 'Algum usuário',
+            email: 'usuario@exemplo.com'
+        })
+
+        await createUserService.execute({
+            id: uuid(),
+            nome: 'Outro usuário',
+            email: 'usuario@exemplo.com'
+        })
+
+        const expectedResponse = [
+            {
+                nome: 'Algum usuário',
+                email: 'usuario@exemplo.com'
+            },
+            {
+                nome: 'Outro usuário',
+                email: 'usuario@exemplo.com'
+            }
+        ]
+        const getAllUserService = new GetAllUserService();
+
+        const result = await getAllUserService.execute();
+
+        expect(result).toMatchObject(expectedResponse);
     })
 })
